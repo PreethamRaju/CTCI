dccd8ddaf80da1f008d96a0b0a7d4b2accd90926
PreethamRaju <pmraju94@gmail.com>
2018-06-04 22:51:35 -0400

    uncommented previous solution

--- ArraysStrings.py
+++ ArraysStrings.py
@@ -4,25 +4,25 @@
 # What if you cannot use data structures.
 
 
-# stringCheck = input("Enter a string to check for duplicates: ").lower()
-# length = len(stringCheck)
-# print(length)
-#
-#
-# def is_duplicate(string):
-#     for i in range(0, int(length)):
-#         for j in range(i+1, length):
-#             if string[i] == string[j]:
-#                 dup = string[i]
-#                 return dup
-#
-#
-# duplicate = is_duplicate(stringCheck)
-#
-# if duplicate:
-#     print("There is a duplicate character = {}".format(duplicate))
-# else:
-#     print("There is no duplicate character")
+stringCheck = input("Enter a string to check for duplicates: ").lower()
+length = len(stringCheck)
+print(length)
+
+
+def is_duplicate(string):
+    for i in range(0, int(length)):
+        for j in range(i+1, length):
+            if string[i] == string[j]:
+                dup = string[i]
+                return dup
+
+
+duplicate = is_duplicate(stringCheck)
+
+if duplicate:
+    print("There is a duplicate character = {}".format(duplicate))
+else:
+    print("There is no duplicate character")
 
 
 # ===============================================================================================
@@ -32,35 +32,35 @@
 # Given two strings. write a method to decide if one is a permutation of the other.
 
 
-# stringOne = input(" Enter the first string: ").lower()
-# stringTwo = input("Enter the second string: ").lower()
-#
-#
-# def is_permutation(strone, strtwo):
-#     lengthone = len(strone)
-#     lengthtwo = len(strtwo)
-#
-#     if lengthone != lengthtwo:
-#         return 0
-#     else:
-#         for charone in strone:
-#             charcheck = 0
-#             for chartwo in strtwo:
-#                 if charone == chartwo:
-#                     charcheck = 1
-#                     break
-#
-#             if charcheck == 0:
-#                 return 0
-#         return 1
-#
-#
-# permutation = is_permutation(stringOne, stringTwo)
-#
-# if permutation == 1:
-#     print("The two strings are permutations")
-# else:
-#     print("The two strings are NOT permutations")
+stringOne = input(" Enter the first string: ").lower()
+stringTwo = input("Enter the second string: ").lower()
+
+
+def is_permutation(strone, strtwo):
+    lengthone = len(strone)
+    lengthtwo = len(strtwo)
+
+    if lengthone != lengthtwo:
+        return 0
+    else:
+        for charone in strone:
+            charcheck = 0
+            for chartwo in strtwo:
+                if charone == chartwo:
+                    charcheck = 1
+                    break
+
+            if charcheck == 0:
+                return 0
+        return 1
+
+
+permutation = is_permutation(stringOne, stringTwo)
+
+if permutation == 1:
+    print("The two strings are permutations")
+else:
+    print("The two strings are NOT permutations")
 
 
 # ==========================================================================
@@ -76,26 +76,26 @@
 # Output: "Mr%20John%20Smith"
 
 
-# string = input("Enter a string: ")
-#
-#
-# def string_insert(strcpy):
-#     length = len(strcpy)
-#     returnstring = ''
-#
-#     for i in range(0, length):
-#         if strcpy[i] == ' ':
-#             returnstring = returnstring + '%20'
-#         else:
-#             returnstring = returnstring + strcpy[i]
-#         if len(returnstring) == length:
-#                 return returnstring
-#     return returnstring
-#
-#
-# result = string_insert(string)
-#
-# print(result)
+string = input("Enter a string: ")
+
+
+def string_insert(strcpy):
+    length = len(strcpy)
+    returnstring = ''
+
+    for i in range(0, length):
+        if strcpy[i] == ' ':
+            returnstring = returnstring + '%20'
+        else:
+            returnstring = returnstring + strcpy[i]
+        if len(returnstring) == length:
+                return returnstring
+    return returnstring
+
+
+result = string_insert(string)
+
+print(result)
 
 # ===============================================================================
 
